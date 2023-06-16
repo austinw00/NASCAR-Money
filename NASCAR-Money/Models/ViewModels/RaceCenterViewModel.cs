e73c278dd6a989087ac73b7561ddd50a99773fbf
Austin Wagner <awagner25@rockets.utoledo.edu>
2023-06-15 22:21:01 -0400

    mostly done race center

--- NASCAR-Money/Models/ViewModels/RaceCenterViewModel.cs
+++ NASCAR-Money/Models/ViewModels/RaceCenterViewModel.cs
@@ -6,7 +6,7 @@ namespace NASCAR_Money.Models.ViewModels
     {
         public Response scheduleCombinedFeed { get; set; }
         public WeekendFeed? weekendFeed { get; set; }
-        public LapAverages? lapAverages { get; set; }
+        //public LapAverages? lapAverages { get; set; }
         public Track? trackInfo { get; set; }
         public RaceListBasic? raceListBasic { get; set; }
     }
--- NASCAR-Money/Services/RaceCenterService.cs
+++ NASCAR-Money/Services/RaceCenterService.cs
@@ -21,14 +21,14 @@ namespace NASCAR_Money.Services
             var _scheduleCombinedFeed = await _cacheService.GetScheduleCombinedFeedAsync(year, seriesId);
             Response _raceWeekend = _scheduleCombinedFeed.response.Where(x => x.Race_Id == raceId).FirstOrDefault();
             var _weekendFeed = await _cacheService.GetWeekendFeedAsync(year, seriesId, raceId);
-            var _lapAverages = await _cacheService.GetLapAveragesAsync(year, seriesId, raceId);
+            //var _lapAverages = await _cacheService.GetLapAveragesAsync(year, seriesId, raceId);
             var _trackInfo = await _context.Tracks.Where(x => x.TrackId == _weekendFeed.weekend_race.FirstOrDefault().track_id).FirstOrDefaultAsync();
             var _raceListBasic = await _cacheService.GetRaceListBasicAsync(year);
             var raceCenterViewModel = new RaceCenterViewModel
             {
                 scheduleCombinedFeed = _raceWeekend,
                 weekendFeed = _weekendFeed,
-                lapAverages = _lapAverages,
+                //lapAverages = _lapAverages,
                 trackInfo = _trackInfo,
                 raceListBasic = _raceListBasic
             };
